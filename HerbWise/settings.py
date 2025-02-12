1305e71edf2dd65401e5f7f2081e0e6fbfe167f0
Ewan Fane <ewanfane2@gmail.com>
2025-02-12 22:27:16 +0000

    allow domain name

--- HerbWise/settings.py
+++ HerbWise/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-^q2!#ls&gd#!)w6-_rqe%h))pg^08aqzaoo&jg+=+f(kmp(dzb
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['213.219.37.35', '127.0.0.1']
+ALLOWED_HOSTS = ['213.219.37.35', '127.0.0.1', 'herbwise.site']
 
 
 # Application definition
